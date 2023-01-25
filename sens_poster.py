72c18be3db2b0f201efa0ace3a229df696462d69
Calvin <98151689+jycal@users.noreply.github.com>
2023-01-25 01:04:58 +0100

    Update sens_poster.py

--- sens_poster.py
+++ sens_poster.py
@@ -1,4 +1,7 @@
-import psutil, time, os, requests
+import time
+import os
+import requests
+from sense_hat import SenseHat
 
 updateInterval = 15  # update once every 15 seconds
 writeAPIkey = "OY8DUS7XDPAU2KTT"  # write API key for the channel
@@ -8,19 +11,25 @@ URL = "https://api.thingspeak.com/update?api_key=OY8DUS7XDPAU2KTT"
 
 
 def getData():
-    """Function that returns the CPU temperature and percentage of CPU utilization"""
+    """Function that returns the temperature and humidity"""
+    sense = SenseHat()
+    sense.clear()
+
+    humid_temp = sense.get_temperature_from_humidity()
+    press_temp = sense.get_temperature_from_pressure()
     cmd = 'cat /sys/class/thermal/thermal_zone0/temp'  # command to get the CPU temperature
     process = os.popen(cmd).readline().strip()
-    cpuTemp = int(process.split('=')[0].split("'")[0]) / 1000 # get the CPU temperature
-    cpuUsage = psutil.cpu_percent(interval=2)  # get the CPU usage
+    cpu_temp = int(process.split('=')[0].split("'")[0]) / 1000 # get the CPU temperature
+    mid_temp = (humid_temp + press_temp) / 2  # avg temp from different sensors
+    temp = mid_temp - ((cpu_temp - mid_temp) / 1.5)
 
-    return cpuTemp, cpuUsage
+    return round(temp)
 
 
 def postData():
-    temp, usage = getData()
+    temp = getData()
 
-    response = requests.post(URL + '&field1=' + str(temp) + '&field2=' + str(usage))
+    response = requests.post(URL + '&field1=' + str(temp))
     if response:
         print('Succes!')
     else:
